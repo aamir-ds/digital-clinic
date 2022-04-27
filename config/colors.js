9e947ef2e47738b26b43704f037d6d27d0397c56
Aamir Mansuri <aamir@digitalsalt.in>
2022-04-27 21:43:20 +0530

    added colors

--- config/colors.js
+++ config/colors.js
@@ -1,5 +1,7 @@
 export default {
     primary: '#6574cf',
     lightGrey: '#f7f9fc',
-    lightPurple: '#c5cee0'
+    lightPurple: '#c5cee0',
+    white: '#ffffff',
+    black: '#000'
 };
--- screens/BookingSuccessful.js
+++ screens/BookingSuccessful.js
@@ -1,27 +1,23 @@
 import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native'
 import React from 'react'
+import colors from '../config/colors'
 
 const BookingSuccessful = () => {
   return (
     <View style={styles.container}>
-
-    <View style={styles.bookingSection}>
-      <View style={styles.roundedImage}>
-        <Image source={require('../assets/images/booking.png')}/>
-      </View>
-
-      <View style={styles.midSection}>
-        <Text style={styles.title}>Booking Successful</Text>
-        <Text style={styles.subtitle}>Your booking has been successful, the reminder is set automatically</Text>
-      </View>
-      <TouchableOpacity style={styles.viewDetails} activeOpacity="0.7">
-        <Text style={styles.link}>View Details</Text>
-      </TouchableOpacity>
-
-    </View>
-      <View>
-        <Button title='Got it' color="#6574CF"></Button>
-      </View>
+        <View style={styles.bookingSection}>
+          <View style={styles.roundedImage}>
+            <Image source={require('../assets/images/booking.png')}/>
+          </View>
+          <View style={styles.midSection}>
+            <Text style={styles.title}>Booking Successful</Text>
+            <Text style={styles.subtitle}>Your booking has been successful, the reminder is set automatically</Text>
+          <TouchableOpacity style={styles.viewDetails} activeOpacity="0.7">
+            <Text style={styles.link}>View Details</Text>
+          </TouchableOpacity>
+          </View>
+        </View>
+        <Button title='Got it' color={colors.primary}/>
     </View>
   )
 }
@@ -31,15 +27,15 @@ export default BookingSuccessful
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
-    padding: 10,
+    backgroundColor: colors.white,
+    padding: 15,
   },
   bookingSection: {
     flex: 1,
     alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'center',
-    width: '80%'
+    width: '100%'
   },
   roundedImage: {
     padding: 35,
@@ -53,26 +49,27 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 30
+    marginVertical: 30
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#000',
+    color: colors.black,
     lineHeight: 30
   },
   subtitle: {
     fontSize: 16,
-    color: '#000',
+    color: colors.black,
     textAlign: 'center',
     lineHeight: 20,
-    marginTop: 10
+    marginTop: 10,
+    marginHorizontal: 15
   },
   viewDetails: {
     marginTop: 20
   },
-  link: {
-    color: '#6574CF',
+  link:{
+    color: colors.primary,
     fontSize: 16,
     fontWeight: 'bold'
   }
