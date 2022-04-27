e9ca7737bba2d9d6e7b2f2491345be5423ff8e18
Aamir Mansuri <aamir@digitalsalt.in>
2022-04-27 15:55:33 +0530

    added dummy data

---
+++ config/dummyData.js
@@ -0,0 +1,182 @@
+const clinics = [
+    {
+        name: 'name of clinic',
+        distance: '68km',
+        address: '987 Blaise Drive Suite 420',
+        ratings: 4.0
+    },
+    {
+        name: 'name of clinic',
+        distance: '68km',
+        address: '987 Blaise Drive Suite 420',
+        ratings: 4.0
+    },
+    {
+        name: 'name of clinic',
+        distance: '68km',
+        address: '987 Blaise Drive Suite 420',
+        ratings: 4.0
+    },
+    {
+        name: 'name of clinic',
+        distance: '68km',
+        address: '987 Blaise Drive Suite 420',
+        ratings: 4.0
+    },
+    {
+        name: 'name of clinic',
+        distance: '68km',
+        address: '987 Blaise Drive Suite 420',
+        ratings: 4.0
+    },
+    {
+        name: 'name of clinic',
+        distance: '68km',
+        address: '987 Blaise Drive Suite 420',
+        ratings: 4.0
+    },
+    {
+        name: 'name of clinic',
+        distance: '68km',
+        address: '987 Blaise Drive Suite 420',
+        ratings: 4.0
+    },
+    {
+        name: 'name of clinic',
+        distance: '68km',
+        address: '987 Blaise Drive Suite 420',
+        ratings: 4.0
+    },
+    {
+        name: 'name of clinic',
+        distance: '68km',
+        address: '987 Blaise Drive Suite 420',
+        ratings: 4.0
+    },
+    {
+        name: 'name of clinic',
+        distance: '68km',
+        address: '987 Blaise Drive Suite 420',
+        ratings: 4.0
+    },
+]
+
+const doctors = [
+    {
+        name: 'Chikanso Chima',
+        type: 'Ophthalmologist',
+        distance: '68km',
+        ratings: 4.0,
+        reviews: 12,
+        video: true,
+        online: true,
+        fees: 125,
+        location: 'New york',
+        experience: 20
+    },
+    {
+        name: 'Chikanso Chima',
+        type: 'Ophthalmologist',
+        distance: '68km',
+        ratings: 4.0,
+        reviews: 12,
+        video: false,
+        online: true,
+        fees: 125,
+        location: 'New york',
+        experience: 20
+    },
+    {
+        name: 'Chikanso Chima',
+        type: 'Ophthalmologist',
+        distance: '68km',
+        ratings: 4.0,
+        reviews: 12,
+        video: true,
+        online: true,
+        fees: 125,
+        location: 'New york',
+        experience: 20
+    },
+    {
+        name: 'Chikanso Chima',
+        type: 'Ophthalmologist',
+        distance: '68km',
+        ratings: 4.0,
+        reviews: 12,
+        video: true,
+        online: true,
+        fees: 125,
+        location: 'New york',
+        experience: 20
+    },
+    {
+        name: 'Chikanso Chima',
+        type: 'Ophthalmologist',
+        distance: '68km',
+        ratings: 4.0,
+        reviews: 12,
+        video: false,
+        online: true,
+        fees: 125,
+        location: 'New york',
+        experience: 20
+    },
+    {
+        name: 'Chikanso Chima',
+        type: 'Ophthalmologist',
+        distance: '68km',
+        ratings: 4.0,
+        reviews: 12,
+        video: false,
+        online: true,
+        fees: 125,
+        location: 'New york',
+        experience: 20
+    },
+]
+
+const paymentMethods=[
+    {
+        name: 'visa'
+    },
+    {
+        name: 'mastercard'
+    },
+    {
+        name: 'paypal'
+    },
+    {
+        name: 'applepay'
+    },
+]
+
+const homePageListing = [
+    {
+        name: 'Doctors',
+        description: 'short description'
+    },
+    {
+        name: 'Clinics',
+        description: 'short description'
+    },
+    {
+        name: 'Specialities',
+        description: 'short description'
+    },
+    {
+        name: 'Labs',
+        description: 'short description'
+    },
+    {
+        name: 'Insurance',
+        description: 'short description'
+    },
+    {
+        name: 'Related Articles',
+        description: 'short description'
+    }
+]
+
+
+export {clinics, doctors, paymentMethods, homePageListing}
