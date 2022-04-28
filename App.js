64c008e5fbd1bddaad027db47399f840a9872732
darshana2496 <darshana@digitalsalt.in>
2022-04-28 17:22:16 +0530

    tab navigation

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home';
+import BottomTabNavigation from './navigation/BottomTabNavigation';
 import AllDoctors from './screens/AllDoctors';
 import BookingSuccessful from './screens/BookingSuccessful';
 import Appointments from './screens/Appointments';
@@ -22,6 +23,7 @@ const App = () => {
         name="BookingSuccessful" 
         component={BookingSuccessful} />
         <Stack.Screen name="Appointments" component={Appointments} />
+        <Stack.Screen name="BottomTabNavigation" component={BottomTabNavigation} />
       </Stack.Navigator>
     </NavigationContainer>
   )
