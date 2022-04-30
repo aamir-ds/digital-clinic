47dc5c79bbb091ad5c7132ab3b0fe8833e488c9d
Aamir Mansuri <aamir@digitalsalt.in>
2022-04-30 11:31:21 +0530

    added vector icons to All doctors screen

--- components/DoctorsList.js
+++ components/DoctorsList.js
@@ -3,10 +3,19 @@ import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react
 import colors from '../config/colors';
 import { clinics, doctors } from '../config/dummy';
 import SortContainer from './SortContainer';
+import Ant from 'react-native-vector-icons/AntDesign';
+import Octi from 'react-native-vector-icons/Octicons';
+import Feather from 'react-native-vector-icons/Feather';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Ion from 'react-native-vector-icons/Ionicons';
+import Fa from 'react-native-vector-icons/FontAwesome';
+
 
 
 
 const Item = ({ name, distance, ratings, image, reviews, type, video }) => (
+
+
     <TouchableOpacity style={styles.item}>
         <View style={{
             flexDirection: 'row',
@@ -24,10 +33,14 @@ const Item = ({ name, distance, ratings, image, reviews, type, video }) => (
                 <View style={{ flex: 1, marginTop: 3 }}>
                     <Text style={{ fontSize: 20, fontWeight: '400', color: colors.black, marginBottom: 5 }}>{name}</Text>
                     <Text style={{ fontSize: 16, fontWeight: '400', color: colors.greyFont, marginBottom: 5 }}>{type}</Text>
-                    <Text style={{ fontSize: 16, fontWeight: '400', color: colors.greyFont }}>{distance} away</Text>
+                    <Text style={{ fontSize: 16, fontWeight: '400', color: colors.greyFont }}>
+                        <Octi size={20} color={colors.greyFont} name={'location'} /> {distance} away</Text>
                 </View>
                 <View style={{ alignItems: 'flex-end' }} >
-                    <Text style={{ fontSize: 17, color: colors.primary, fontWeight: '500' }}>{ratings}</Text>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Fa style={{ marginRight: 5 }} size={15} color={colors.primary} name={'star'} />
+                        <Text style={{ fontSize: 17, color: colors.primary, fontWeight: '500' }}>{ratings}</Text>
+                    </View>
                     <Text style={{ fontSize: 14, color: colors.greyFont, }}>({reviews})</Text>
                 </View>
 
@@ -42,17 +55,12 @@ const Item = ({ name, distance, ratings, image, reviews, type, video }) => (
             paddingTop: 10
         }}>
             <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
-                <View style={{ height: 30, width: 30, borderColor: colors.lightGrey, borderWidth: 3, justifyContent: 'center', alignItems: 'center', borderRadius: 5, marginRight: 10 }}>
-                    <Text>H</Text>
-                </View>
+                {video ? <Feather style={{ marginRight: 10 }} size={20} color={colors.primary} name={'video'} /> :
+                    <Feather style={{ marginRight: 10 }} size={20} color={colors.greyFont} name={'video-off'} />}
                 <Text style={{ color: video ? colors.primary : colors.greyFont, fontSize: 16 }}>Video Visit</Text>
             </View>
-            <View style={{ height: 30, width: 30, borderColor: colors.lightGrey, borderWidth: 3, justifyContent: 'center', alignItems: 'center', borderRadius: 5, marginRight: 10 }}>
-                <Text>H</Text>
-            </View>
-            <View style={{ height: 30, width: 30, borderColor: colors.lightGrey, borderWidth: 3, justifyContent: 'center', alignItems: 'center', borderRadius: 5 }}>
-                <Text>H</Text>
-            </View>
+            <Entypo style={{ marginRight: 10 }} size={20} color={colors.greyFont} name={'calendar'} />
+            <Ion size={20} color={colors.greyFont} name={'ios-chatbubble-ellipses-outline'} />
         </View>
 
     </TouchableOpacity>
--- components/SortContainer.js
+++ components/SortContainer.js
@@ -1,16 +1,16 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import colors from '../config/colors'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 const SortContainer = () => {
     return (
         <View style={{ alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row', marginTop: 20, marginBottom: 10, marginHorizontal: 20 }}>
             <Text style={{ color: colors.black, fontSize: 16 }}>Doctor List</Text>
-            <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'baseline', justifyContent: 'space-between' }}>
-                <Text style={{ color: colors.greyFont }}>Sort View</Text>
-                <View style={{ height: 30, width: 30, borderColor: colors.lightGrey, borderWidth: 3, justifyContent: 'center', alignItems: 'center', borderRadius: 5, marginLeft: 10 }}>
-                    <Text>H</Text>
-                </View>
+            <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                <Text style={{ color: colors.greyFont, marginRight: 6 }}>Sort View</Text>
+                <MaterialCommunityIcons size={25} color={colors.greyFont} name={'view-quilt-outline'} />
             </TouchableOpacity>
         </View >
     )
