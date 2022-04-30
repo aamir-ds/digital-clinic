3b266b89e58e427734af9b655c6d93a2bb0f7acc
darshana2496 <darshana@digitalsalt.in>
2022-04-30 11:01:35 +0530

    stack navigation common page

--- App.js
+++ App.js
@@ -1,67 +1,17 @@
 import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './screens/Home';
-import BottomTabNavigation from './navigation/BottomTabNavigation';
-import AllDoctors from './screens/AllDoctors';
-import BookingSuccessful from './screens/BookingSuccessful';
-import DoctorProfile from './screens/DoctorProfile';
-import Appointments from './screens/Appointments';
-import Clinics from './screens/Clinics';
-import colors from './config/colors';
-import Tabs from './navigation/tabs';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import StackNavigation from './navigation/StackNavigation';
 
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="DoctorProfile">
-
-        <Stack.Screen
-          options={{
-            headerShown: false
-          }}
-          name="Home" component={Tabs} />
-
-        <Stack.Screen
-          options={{
-            headerShown: false
-          }}
-          name="All Doctors" component={AllDoctors} />
-
-        <Stack.Screen
-          options={{
-            headerShown: false
-          }}
-          name="BookingSuccessful"
-          component={BookingSuccessful} />
-
-        <Stack.Screen
-          options={{
-            headerShown: false
-          }}
-          name="Appointments" component={Appointments} />
-
-        <Stack.Screen
-          options={{
-            headerStyle: {
-              backgroundColor: colors.primary,
-            },
-            headerTintColor: colors.white,
-            headerTitleStyle: {
-              fontWeight: '400',
-              fontSize: 20
-            },
-          }}
-          name="Clinics" component={Clinics} />
-        <Stack.Screen name="BottomTabNavigation" component={BottomTabNavigation} />
-        <Stack.Screen name="DoctorProfile" component={DoctorProfile} options={{
-          headerTitle:"Doctor's Profile"
-        }} />
-      </Stack.Navigator>
+      <StackNavigation />
     </NavigationContainer>
   )
 }
---
+++ navigation/StackNavigation.js
@@ -0,0 +1,66 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from '../screens/Home';
+import BottomTabNavigation from './BottomTabNavigation';
+import AllDoctors from '../screens/AllDoctors';
+import BookingSuccessful from '../screens/BookingSuccessful';
+import DoctorProfile from '../screens/DoctorProfile';
+import Appointments from '../screens/Appointments';
+import Clinics from '../screens/Clinics';
+import colors from '../config/colors';
+import Tabs from './tabs';
+
+const Stack = createNativeStackNavigator();
+
+export default function StackNavigation() {
+  return (
+    <Stack.Navigator initialRouteName="Tabs">
+
+    <Stack.Screen
+      options={{
+        headerShown: false
+      }}
+      name="Home" component={Tabs} />
+
+    <Stack.Screen
+      options={{
+        headerShown: false
+      }}
+      name="All Doctors" component={AllDoctors} />
+
+    <Stack.Screen
+      options={{
+        headerShown: false
+      }}
+      name="BookingSuccessful"
+      component={BookingSuccessful} />
+
+    <Stack.Screen
+      options={{
+        headerShown: false
+      }}
+      name="Appointments" component={Appointments} />
+
+    <Stack.Screen
+      options={{
+        headerStyle: {
+          backgroundColor: colors.primary,
+        },
+        headerTintColor: colors.white,
+        headerTitleStyle: {
+          fontWeight: '400',
+          fontSize: 20
+        },
+      }}
+      name="Clinics" component={Clinics} />
+    <Stack.Screen name="BottomTabNavigation" component={BottomTabNavigation} />
+    <Stack.Screen name="DoctorProfile" component={DoctorProfile} options={{
+      headerTitle:"Doctor's Profile"
+    }} />
+  </Stack.Navigator>
+  )
+}
+
+const styles = StyleSheet.create({})
